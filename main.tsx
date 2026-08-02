f4ee6c503ac5d8de651fa3d3c5defa9f9fc0b284
almd-blip <isildalmd@gmail.com>
2026-08-02 06:28:12 +0100

    fix: update main.tsx imports to match flattened files (CmsContentProvider, ErrorBoundary)

--- main.tsx
+++ main.tsx
@@ -1,8 +1,8 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import { CmsContentProvider } from './cms/CmsContentProvider';
-import { ErrorBoundary } from './components/ErrorBoundary';
+import { CmsContentProvider } from './CmsContentProvider';
+import { ErrorBoundary } from './ErrorBoundary';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
